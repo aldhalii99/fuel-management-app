5835f3f23580ed3ec84c81318c5a5d0716ad2658
aldhalii99 <githubaldhali@gmail.com>
2026-02-07 10:35:16 -1100

    Initial commit - مشروع إدارة المحروقات

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('انس عبدة صالح حسين'),
+      ),
+      body: const SizedBox(),
+    );
+  }
+}
